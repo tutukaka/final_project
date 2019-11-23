d46e021883e54432e6e6a1a292bf1cdeb083c24d
tutukaka <dikstasisb12@gmail.com>
2019-11-23 16:39:40 +0300

    изменил путь до картинок, добавил запрос картинок с сервера и вывод их на страницу

--- js/global.js
+++ js/global.js
@@ -2,5 +2,15 @@ const app = new Vue({
     el: '.global',
     data: {
         message: 12
-    }
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                    // this.$refs.error.setError(error);
+                })
+        },
+    },
 });
--- js/productList.js
+++ js/productList.js
@@ -1,37 +1,50 @@
-Vue.component('catalog', {
-    props: [''],
-    methods: {
-        handleSearchClick(value) {
-            const regexp = new RegExp(value, 'i');
-            this.filteredItems = this.items.filter((item) => {
-                return regexp.test(item.title);
-            });
-        },
+const catalogItem = {
+    props: ['item'],
+    template: `
+    <div class="product__common bxbb">
+        <div class="product__common_img flex"><div class="product__common_img_style flex">
+            <img :src="item.img" alt="photo"></div>
+        </div>
+        <div class="product__common_text">{{ item.title }}<br>
+            <span class="product__common_color">$&nbsp;{{ item.price }}</span>
+            <div class="product__common_hover bxbb">
+                <a href="single_page.html" class="product__common_hover_a flex"></a>
+                <div class="product__common_button flex">
+                    <a class="product__common_button_a flex" href="#">
+                    <img class="product__common_button_img"
+                         src="img/hover_product.svg" alt="ico">Add to Cart</a></div>
+            </div>
+        </div>
+    </div>
+    `
+};
 
+Vue.component('catalog', {
+    data(){
+        return {
+            items: []
+        }
+    },
+    components: {
+        'catalog-item': catalogItem,
+    },
+    methods: {
     },
     mounted(){
-        this.$parent.getJson('/goods')
+        this.$parent.getJson('https://raw.githubusercontent.com/tutukaka/final_project/first_try/catalog.json')
             .then((goods) => {
                 this.items = goods;
-                this.filteredItems = goods;
             });
     },
     template: `
-    <div class="product__common bxbb">
-                    <div class="product__common_img flex"><div class="product__common_img_style flex">
-                        <img class="product__common_img_style" src="img/product_1.jpg" alt="photo"></div>
-                    </div>
-                    <div class="product__common_text">10 Mango People T-shirt <br>
-                        <span class="product__common_color">$52.00</span>
-                        <div class="product__common_hover bxbb">
-                            <a href="single_page.html" class="product__common_hover_a flex"></a>
-                            <div class="product__common_button flex">
-                                <a class="product__common_button_a flex" href="#">
-                                <img class="product__common_button_img"
-                                     src="img/hover_product.svg" alt="ico">Add to Cart</a></div>
-                        </div>
-                    </div>
-                </div>
+    <div class="product__list container flex">
+        <catalog-item v-for="item in items"
+            :key="item.id"
+            :item="item"
+        ></catalog-item>
+    </div>
     `
 });
+
+
 
